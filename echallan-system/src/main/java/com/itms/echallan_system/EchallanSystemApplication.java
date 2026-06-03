f883af0659ff7ac2d71bbe5892580a53c6a485f9
Anant Dubey <work.anantdubey@gmail.com>
2026-06-03 14:29:04 +0530

    Initial Spring Boot setup

---
+++ echallan-system/src/main/java/com/itms/echallan_system/EchallanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.itms.echallan_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchallanSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchallanSystemApplication.class, args);
+	}
+
+}
